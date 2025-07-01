eb5d32c7a512c7e322313e519f2240023788de6d
cloner174 <hamedhajipour1400@gmail.com>
2025-07-02 01:27:06 +0330

    new script for new model

---
+++ script_downstream_wlasl_with_dynamic_weighter.sh
@@ -0,0 +1,19 @@
+export TF_ENABLE_ONEDNN_OPTS=0
+
+CUDA_VISIBLE_DEVICES=0 python downstream_classification.py \
+  --lr 0.01 \
+  --batch-size 32 \
+  --pretrained 'save_ckpt_pretrain\checkpoint_0150.pth.tar' \
+  --finetune-dataset SLR \
+  --protocol cross_subject \
+  --data-ratio 1.0 \
+  --finetune-skeleton-representation graph-based \
+  --checkpoint-path save_ckpt_downstream \
+  --optim SGD \
+  --subset_name WLASL \
+  --num_class 100 \
+  --input_size 64 \
+  --eval_step 1 \
+  --view all \
+  --save-ckpt \
+  --use_dynamic_weighter
